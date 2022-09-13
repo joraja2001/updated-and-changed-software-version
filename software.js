c3ccaaac031793b278b279467fd3653d5eb1d84f
joraja2001 <jothikajo96@gmail.com>
2022-09-13 11:39:49 +0530

    this is the final one for the software version in a separate repo

--- software.js
+++ software.js
@@ -1,1 +1,196 @@
+console.log('____________________________________________JAVASCRIPT_______________________________')
+software_UPDATES=[
+        {
+
+        Version:'2.1',
+       Release_date:'2021',
+        Bugs:"crashing",
+        Feature:"karaoke mode",
+        Author:"spotify",
+        Type:"major"
+    
+    },
+    {
+           Version:'3.1',
+           Release_date:'2022',
+           Bugs:"UPI bug",
+           Feature:"introducing to action calls",
+           Author:"truecaller",
+           Type:"major"
+    
+    },
+    {
+    Version:'4.1',
+    Release_date:'2022',
+    Bugs:"video freeze",
+    Feature:"icons get new look",
+    Author:"netflix" ,
+    Type:"major"  
+    },
+{
+    Version:'4.3',
+    Release_date:'2021',
+    Bugs:'voice call error',
+    Feature:"admin msg control",
+    Author:"whatsapp",
+    Type:"match enhancement"
+},
+{
+    Version:'4.5',
+    Release_date:'2022',
+    Bugs:'crashing',
+    Feature:"react over msg",
+    Author:"whatsapp",
+    Type:"match enhancement"
+},
+{ Version:'4.7',
+    Release_date:'2022',
+    Bugs:'crashing',
+    Feature:"more reaction over msg",
+    Author:"whatsapp",
+    Type:"match enhancement"
+
+}]
+console.table(software_UPDATES)
+Bugs=[
+    {BugID:'101',
+        Name:'spotify',
+        Version:'2.1',
+       Release_date:'21.12.2021',
+        Bugs:"crashing",
+        Feature:"karaoke mode",
+        Author:["spotify"],
+        Type:"major"
+    },
+    {BugID:'102',
+        Name:'truecaller',
+        Version:'3.1',
+        Release_date:'28.06.2022',
+        Bugs:"UPI bug",
+        Feature:"introducing to action calls",
+        Author:["truecaller"],
+        Type:"major"
+    },
+
+    {
+        BugID:'103',
+        Name:'netflix',
+    Version:'4.1',
+    Release_date:'12.09.2022',
+    Bugs:"video freeze",
+    Feature:"icons get new look",
+    Author:["netflix"] ,
+    Type:"major"
+    },
+
+    {BugID:'104',
+    Name:'whatsapp',
+    Version:'4.3',
+    Release_date:'25.04.2021',
+    Bugs:'voice call error',
+    Feature:"admin msg control",
+    Author:["Jan Koum"],
+    Type:"match enhancement"
+    },
+
+    {
+     BugID:'105',
+     Name:'whatsapp',
+    Version:'4.5',
+    Release_date:'07.08.2022',
+    Bugs:'crashing',
+    Feature:"react over msg",
+    Author:["Brian"],
+    Type:"match enhancement"
+    },
+
+    {
+        BugID:'106',
+        Name:'whatsapp',
+        Version:'4.7',
+        Release_date:'05.10.2022',
+        Bugs:'crashing',
+        Feature:"more reaction over msg",
+        Author:["Brian", "Acton" ,"Jan Koum"],
+        Type:"match enhancement"
+
+    }
+]
+
+    function searchbyyear(year){
+
+    count_UPDATES=software_UPDATES.filter(n=>n.Release_date.includes(year))
+    console.log(`${count_UPDATES.length} "versions came in the year of ${year}`)
+    console.table(count_UPDATES)
+    }
+
+    searchbyyear('2022')
+
+
+    console.log('__________________________________________________________________________________________________________________________')
+   
+
+    function searchbyspecificbug(BugID){
+        count_bug=Bugs.filter(n=>n.BugID.includes(BugID))
+        console.log(`"this is the ${BugID} bug occured for the below release `)
+        console.table(count_bug)
+    }
+    searchbyspecificbug('101')
+    console.log('__________________________________________________________________________________________________________________________')
+   
+
+
+    function searchbyauthor(Name){
+
+        count_AUTHOR=software_UPDATES.filter(n=>n.Author.includes(Name))
+        console.log(`${Name} is the one who launched  ${count_AUTHOR.length} times software updates `)
+        console.log(count_AUTHOR)
+    }
+    searchbyauthor('whatsapp')
+
+    console.log('__________________________________________________________________________________________________________________________')
+   
+
+    authors_OFSOFTWARES=["spotify","truecaller","netflix","Jan Koum","Brian","Brian", "Brian" ,"Jan Koum"]
+    majorauthor=1;
+    time0fAuthor=0;
+    for(author=0;author< authors_OFSOFTWARES.length;author++){
+        for(authors=1;authors< authors_OFSOFTWARES.length;authors++){
+            if(authors_OFSOFTWARES[author]===authors_OFSOFTWARES[authors])
+            time0fAuthor++;
+        
+        if(majorauthor<time0fAuthor){
+            majorauthor=time0fAuthor;
+            Author= authors_OFSOFTWARES[author]
+        }
+    }
+    time0fAuthor=0;
+}
+console.table(`${ Author} is the one who is an  major author of performing ,${majorauthor} times`)
+
+    console.log('__________________________________________________________________________________________________________________________')
+   
+
+    function searchbyversion(version){
+
+        version_FORSOFTWARE=software_UPDATES.filter(n=>n.Version.includes(version))
+        console.log(`"the software which having  ${version}version  `)
+        console.table(version_FORSOFTWARE)    
+    }
+    
+    searchbyversion('4.1')
+    console.log('__________________________________________________________________________________________________________________________')
+    
+   
+    function searchbytype(type){
+        type_OFSOFTWARE=software_UPDATES.filter(n=>n.Type.includes(type))
+        console.log(`"the software which having  ${type} type  `)
+        console.table(type_OFSOFTWARE)
+    }
+    console.log('__________________________________________________________________________________________________________________________')
+    searchbytype('match enhancement')
+    console.log('__________________________________________________________________________________________________________________________')
+    searchbytype('major')
+
+
 
--- software.ts
+++ software.ts
@@ -1,1 +1,212 @@
+console.log("________________________________________TYPESCRIPT__________________________________________")
+interface Version{
+    Name:string,
+    Version:string,
+   Release_date:string,
+    Bugs:string,
+    Feature:string,
+    Author:string[]|string,
+    Type:string
+}
+interface Bugs{
+    BugID:string,
+    Name:string,
+    Version:string,
+   Release_date:string,
+    Bugs:string,
+    Feature:string,
+    Author:string[]|string,
+    Type:string
+}
 
+
+const software_UPDATES:Version[]=[
+    {
+    Name:'spotify',
+    Version:'2.1',
+   Release_date:'21.12.2021',
+    Bugs:"crashing",
+    Feature:"karaoke mode",
+    Author:["spotify"],
+    Type:"major"
+
+},
+{      Name:'truecaller',
+       Version:'3.1',
+       Release_date:'28.06.2022',
+       Bugs:"UPI bug",
+       Feature:"introducing to action calls",
+       Author:["truecaller"],
+       Type:"major"
+
+},
+{
+Name:'netflix',
+Version:'4.1',
+Release_date:'12.09.2022',
+Bugs:"video freeze",
+Feature:"icons get new look",
+Author:["netflix"] ,
+Type:"major"  
+},
+{
+Name:'whatsapp',
+Version:'4.3',
+Release_date:'25.04.2021',
+Bugs:'voice call error',
+Feature:"admin msg control",
+Author:["Jan Koum"],
+Type:"match enhancement"
+},
+{   Name:'whatsapp',
+Version:'4.5',
+Release_date:'07.08.2022',
+Bugs:'crashing',
+Feature:"react over msg",
+Author:["Brian"],
+Type:"match enhancement"
+},
+{   Name:'whatsapp',
+Version:'4.7',
+Release_date:'05.10.2022',
+Bugs:'crashing',
+Feature:"more reaction over msg",
+Author:["Brian", "Acton" ,"Jan Koum"],
+Type:"match enhancement"
+}]
+console.log(software_UPDATES)
+const Bugs:Bugs[]=[
+{
+    BugID:'101',
+    Name:'spotify',
+    Version:'2.1',
+   Release_date:'21.12.2021',
+    Bugs:"crashing",
+    Feature:"karaoke mode",
+    Author:["spotify"],
+    Type:"major"
+},
+{BugID:'102',
+    Name:'truecaller',
+    Version:'3.1',
+    Release_date:'28.06.2022',
+    Bugs:"UPI bug",
+    Feature:"introducing to action calls",
+    Author:["truecaller"],
+    Type:"major"
+},
+{
+    BugID:'103',
+    Name:'netflix',
+    Version:'4.1',
+    Release_date:'12.09.2022',
+    Bugs:"video freeze",
+    Feature:"icons get new look",
+    Author:["netflix"] ,
+    Type:"major"
+},
+
+{BugID:'104',
+Name:'whatsapp',
+Version:'4.3',
+Release_date:'25.04.2021',
+Bugs:'voice call error',
+Feature:"admin msg control",
+Author:["Jan Koum"],
+Type:"match enhancement"
+},
+
+{
+ BugID:'105',
+ Name:'whatsapp',
+Version:'4.5',
+Release_date:'07.08.2022',
+Bugs:'crashing',
+Feature:"react over msg",
+Author:["Brian"],
+Type:"match enhancement"
+},
+
+{
+    BugID:'106',
+    Name:'whatsapp',
+    Version:'4.7',
+    Release_date:'05.10.2022',
+    Bugs:'crashing',
+    Feature:"more reaction over msg",
+    Author:["Brian", "Acton" ,"Jan Koum"],
+    Type:"match enhancement"
+
+}
+]
+
+function searchByYear(year:string){
+var count_UPDATES=software_UPDATES.filter(n=>n.Release_date.includes(year))
+console.log(`${count_UPDATES.length} "versions came in the year of ${year}`)
+console.log(count_UPDATES)
+}
+searchByYear('2021')
+console.log('__________________________________________________________________________________________________________________________')
+
+
+function searchBySpecificbug(BugID:string){
+   var count_bug=Bugs.filter(n=>n.BugID.includes(BugID))
+    console.log(`"this is the ${BugID} bug occured for the below release `)
+    console.log(count_bug)
+}
+searchBySpecificbug('101')
+console.log('__________________________________________________________________________________________________________________________')
+
+function searchByauthor(name:string){
+   var count_AUTHOR=software_UPDATES.filter(n=>n.Author.includes(name))
+    console.log(`"${name} is the one who is part of software updates for  ${count_AUTHOR.length} times `)
+    console.log(count_AUTHOR)
+}
+searchByauthor("Jan Koum")
+
+console.log('__________________________________________________________________________________________________________________________')
+
+var authors_OFSOFTWARES=["spotify","truecaller","netflix","Jan Koum","Brian","Brian", "Brian" ,"Jan Koum"]
+var majorauthor=1;
+var timeOfauthor=0;
+for(let Author=0;Author< authors_OFSOFTWARES.length;Author++){
+    for(let Times=1;Times< authors_OFSOFTWARES.length;Times++){
+        if(authors_OFSOFTWARES[Author]===authors_OFSOFTWARES[Times])
+     timeOfauthor++;
+    
+    if(majorauthor<timeOfauthor){
+        majorauthor=timeOfauthor;
+       var person:any= authors_OFSOFTWARES[Author]
+    }
+}
+timeOfauthor=0;
+}
+console.log(`${person} is the one who is an  major author of performing ,${majorauthor} times`)
+
+console.log('__________________________________________________________________________________________________________________________')
+
+
+function searchByVersion(Version:string){
+    
+   var version_FORSOFTWARE=software_UPDATES.filter(n=>n.Version.includes(Version))
+   
+    console.log(`"the software which having  ${Version}Version  `)
+    console.log(version_FORSOFTWARE)
+  
+    
+}
+
+searchByVersion('4.1')
+
+console.log('__________________________________________________________________________________________________________________________')
+
+
+function searchByType(Type:string){
+   var type_OFSOFTWARE=software_UPDATES.filter(n=>n.Type.includes(Type))
+    console.log(`"the software which having  ${Type} type  `)
+    console.log(type_OFSOFTWARE)
+}
+console.log('__________________________________________________________________________________________________________________________')
+searchByType('match enhancement')
+console.log('__________________________________________________________________________________________________________________________')
+searchByType('major')
